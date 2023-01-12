b4afa178be54821508f6b5680b3862cf5f20634f
devlsl <vslozhen@gmail.com>
2023-01-12 11:32:51 +0300

    Finish optimized resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -19,31 +19,43 @@ export class Table extends ExcelComponent {
     if (event.target.dataset.resize) {
       const $resizer = $(event.target)
       const $parent = $resizer.closest('[data-type="resizable"]')
-      const coords = $parent.getCoords()
 
+      const startPositionX = event.clientX
+      const startPositionY = event.clientY
       const type = $resizer.data.resize
 
       const colNum = $parent.data.col
       const cellsInCurrentCol = this.$root.findAll(
-        `.row:not(:nth-child(1)) [data-col="${colNum}"]`
+        `[data-col="${colNum}"]`
       )
+
       document.onmousemove = e => {
         if (type === 'col') {
-          const delta = e.clientX - coords.right
-          const value = coords.width + delta
-          $parent.css({width: value + 'px'})
-          cellsInCurrentCol.forEach(cell => {
-            cell.style.width = value + 'px'
-          })
+          const finishPositionX = e.clientX
+          const delta = finishPositionX - startPositionX
+          $resizer.css({'right': -delta + 'px', 'z-index': '99', 'opacity': '1', 'bottom': '-5000px'})
         } else {
-          const delta = e.clientY - coords.bottom
-          const value = coords.height + delta
-          $parent.css({height: value + 'px'})
+          const finishPositionY = e.clientY
+          const delta = finishPositionY - startPositionY
+          $resizer.css({'bottom': -delta + 'px', 'z-index': '99', 'opacity': '1', 'right': '-5000px'})
         }
       }
 
       document.onmouseup = () => {
+        if (type === 'col') {
+          const value = $resizer.getCoords().right - $parent.getCoords().left
+          $parent.css({width: value + 'px'})
+          cellsInCurrentCol.forEach(cell => {
+            cell.style.width = value + 'px'
+          })
+          $resizer.css({'right': '', 'z-index': '', 'opacity': '', 'bottom': ''})
+        } else {
+          const value = $resizer.getCoords().bottom - $parent.getCoords().top
+          $parent.css({height: value + 'px'})
+          $resizer.css({'bottom': '', 'z-index': '', 'opacity': '', 'right': ''})
+        }
         document.onmousemove = null
+        document.onmouseup = null
       }
 
     }
@@ -52,51 +64,3 @@ export class Table extends ExcelComponent {
 
 }
 
-// import {ExcelComponent} from '@core/ExcelComponent'
-// import {createTable} from '@/components/table/table.template'
-// import {$} from '@core/dom'
-//
-// export class Table extends ExcelComponent {
-//   static className = 'excel__table'
-//
-//   constructor($root) {
-//     super($root, {
-//       listeners: ['mousedown']
-//     })
-//   }
-//
-//   toHTML() {
-//     return createTable(20)
-//   }
-//
-//   onMousedown(event) {
-//     if (event.target.dataset.resize) {
-//       const $resizer = $(event.target)
-//       const $parent = $resizer.closest('[data-type="resizable"]')
-//       const coords = $parent.getCoords()
-//
-//       const colNum = $parent.data.col
-//       const cellsInCurrentCol = document.querySelectorAll(
-//         `.excel__table .row:not(:nth-child(1)) [data-col="${colNum}"]`
-//       )
-//       // console.log(cellsInCurrentCol)
-//
-//       document.onmousemove = e => {
-//         const delta = e.pageX - coords.right
-//         const value = coords.width + delta
-//         $parent.$el.style.width = value + 'px'
-//         cellsInCurrentCol.forEach(cell => {
-//           cell.style.width = value + 'px'
-//           console.log('1')
-//         })
-//       }
-//
-//       document.onmouseup = () => {
-//         document.onmousemove = null
-//       }
-//
-//     }
-//   }
-//
-//
-// }
